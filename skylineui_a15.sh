94054caed516395033f9c7c443979f463ee9229f
DarkKiller28 <oyunbaskani7728@gmail.com>
2025-06-24 11:44:08 +0300

    Create skylineui_a15.sh

---
+++ skylineui_a15.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/SkylineUI-Reborn/platform_manifest.git -b fifteen --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/DarkKiller28/local_manifest.git .repo/local_manifests -b sweet2-pixelos
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=DarkKiller 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+lunch aosp_sweet2-bp1a-userdebug
+
+# Build
+mka skyline -j$(nproc --all)
+
