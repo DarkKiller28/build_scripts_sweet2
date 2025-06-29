62391f6759fdae1dafb74de17b3e3cdd6fc3ba07
DarkKiller28 <oyunbaskani7728@gmail.com>
2025-06-29 13:22:11 +0300

    Create sakura_a15.sh

---
+++ sakura_a15.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 15 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/DarkKiller28/local_manifest.git .repo/local_manifests -b sweet2-sakura
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=DarkKiller 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+lunch lineage_sweet2-ap4a-userdebug
+
+# Build
+mka bacon
