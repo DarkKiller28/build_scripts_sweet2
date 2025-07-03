716180c61fe951e5f2479fc7658e95d4a7a057df
DarkKiller28 <oyunbaskani7728@gmail.com>
2025-07-03 14:28:15 +0300

    Create evo_a15.sh

---
+++ evo_a15.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/DarkKiller28/local_manifest.git .repo/local_manifests -b sweet2-evo
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=DarkKiller 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+lunch lineage_sweet2-bp1a-userdebug
+
+# Build
+m evolution
