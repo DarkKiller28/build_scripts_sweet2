2e237879e3706ffefd1b5361373c291b630be65f
Soner <oyunbaskani7728@gmail.com>
2025-09-20 01:27:21 +0300

    Create clover_a16.sh

---
+++ clover_a16.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/The-Clover-Project/manifest.git -b 16 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/DarkKiller28/local_manifest.git .repo/local_manifests -b sweet2-clover
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=DarkKiller 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+lunch clover_sweet2-bp2a-userdebug
+
+# Build
+mka clover -j$(nproc --all)
