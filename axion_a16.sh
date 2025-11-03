bd16bc1c2fa9a88e994e29c0b91230c1c4405517
Soner <oyunbaskani7728@gmail.com>
2025-11-03 20:38:02 +0300

    Add axion_a16.sh script for build setup

---
+++ axion_a16.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/DarkKiller28/local_manifest.git .repo/local_manifests -b sweet2-axionaosp-23.0
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+/opt/crave/resync.sh 
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=DarkKiller 
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "============="
+
+# Lunch
+breakfast sweet2 gms pico
+
+# Build
+mka bacon
