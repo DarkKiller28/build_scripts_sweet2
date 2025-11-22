98086d809022b87c20f27167c6904690424831f3
Soner <oyunbaskani7728@gmail.com>
2025-11-22 21:21:05 +0300

    Update clover_a16.sh

--- clover_a16.sh
+++ clover_a16.sh
@@ -15,7 +15,7 @@ echo "Local manifest clone success"
 echo "============================"
 
 # Build Sync
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) 
+/opt/crave/resync.sh
 echo "============="
 echo "Sync success"
 echo "============="
