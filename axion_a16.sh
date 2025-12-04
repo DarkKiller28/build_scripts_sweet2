0afb8340e4687c38418928313a64cde2a721cdc0
Soner <oyunbaskani7728@gmail.com>
2025-12-04 19:24:29 +0300

    Update axion_a16.sh

--- axion_a16.sh
+++ axion_a16.sh
@@ -20,6 +20,10 @@ echo "============="
 echo "Sync success"
 echo "============="
 
+echo ">> Cloning custom Soong"
+rm -rf build/soong
+git clone https://github.com/DarkKiller28/android_build_soong build/soong
+
 # Export
 export BUILD_USERNAME=DarkKiller 
 export BUILD_HOSTNAME=crave
