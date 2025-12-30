1cf5e85ff4c2f4a32d7c967f2c93c8cc202b776b
Soner <oyunbaskani7728@gmail.com>
2025-12-30 21:55:41 +0300

    Update lineage_a16.sh

--- lineage_a16.sh
+++ lineage_a16.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.1 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/DarkKiller28/local_manifest.git .repo/local_manifests -b sweet2-lineage
+git clone https://github.com/DarkKiller28/local_manifest.git .repo/local_manifests -b sweet2-crdroid16
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -26,15 +26,13 @@ export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 echo "======= Export Done ======"
 
-# Delete Error Line
-sed -i '/type lirc_device, dev_type;/d' device/lineage/sepolicy/common/vendor/device.te
 
 # Set up build environment
 source build/envsetup.sh
 echo "============="
 
 # Lunch
-lunch lineage_sweet2-bp2a-userdebug
+lunch lineage_sweet2-bp3a-eng
 
 # Build
 mka bacon
