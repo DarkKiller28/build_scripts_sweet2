1de3fe02040ebed61d0062fb90fdf6694a795d10
Soner <darkkiller2877@gmail.com>
2026-05-15 08:32:24 +0300

    Update clover_a16.sh

--- clover_a16.sh
+++ clover_a16.sh
@@ -15,6 +15,9 @@ echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
+# Clover Hardware
+git clone https://github.com/The-Clover-Project/hardware_xiaomi.git -b 16-qpr2 hardware/xiaomi
+
 # Build Sync
 /opt/crave/resync.sh
 echo "============="
